55e6da327144838cf17528ca3efc0d78bec358c9
Gladky Dmitry <miktor.ru@gmail.com>
2018-07-30 00:38:38 +0300

    fix solution

--- fast_stl/fast_stl.py
+++ fast_stl/fast_stl.py
@@ -3,9 +3,10 @@ from fast_stl_impl import loess
 
 
 def test():
-    loess(np.arange(0, 10, 1, dtype=np.float32), np.arange(0, 10, 1, dtype=np.float32), np.arange(0, 6, 1, dtype=np.float32), 4)
+    return loess(np.arange(0, 10, 1, dtype=np.float32), np.arange(0, 10, 1, dtype=np.float32), np.arange(0, 6, 1, dtype=np.float32), 4)
 
 
 if __name__ == "__main__":
     print('Running benchmarks')
-    test()
+    res = test()
+    print(res)
