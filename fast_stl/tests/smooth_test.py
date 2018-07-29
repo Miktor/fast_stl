f27d5e03f837f15ef63c7dcc5f5b8a71d2a8cd09
Gladky Dmitry <miktor.ru@gmail.com>
2018-07-30 01:05:03 +0300

    fix build steps

--- fast_stl/tests/smooth_test.py
+++ fast_stl/tests/smooth_test.py
@@ -1,9 +1,12 @@
 import unittest
 import numpy as np
-from fast_stl_impl import kernel_smoothing
+from fast_stl.fast_stl_impl import kernel_smoothing
 
 
 class TestInputs(unittest.TestCase):
     def test_numpy_inputs(self):
+        x = np.arange(0, 10, 1, dtype=np.float32)
+        y = np.arange(0, 10, 1, dtype=np.float32)
+        kernel_smoothing(x, y)
         pass
 
