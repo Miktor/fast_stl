4e51e8c8bb2573999c2d868b072d90fcab562374
Gladky Dmitry <miktor.ru@gmail.com>
2018-07-30 04:49:30 +0300

    cleanup, resource managment and different tpyes support

--- fast_stl/src/fast_stl_impl.cpp
+++ fast_stl/src/fast_stl_impl.cpp
@@ -1,12 +1,50 @@
 #include <Python.h>
+#include <utility>
 #include <numpy\ndarraytypes.h>
 #include <numpy\arrayobject.h>
 #include <numpy\npy_math.h>
 #include "loess.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+template <NPY_TYPES Type>
+struct PythonTypeHelper {};
+
+template <>
+struct PythonTypeHelper<NPY_FLOAT>
+{
+	typedef float Type;
+};
+template <>
+struct PythonTypeHelper<NPY_CFLOAT>
+{
+	typedef float Type;
+};
+template <>
+struct PythonTypeHelper<NPY_DOUBLE>
+{
+	typedef double Type;
+};
+template <>
+struct PythonTypeHelper<NPY_CDOUBLE>
+{
+	typedef double Type;
+};
+
+template <NPY_TYPES NpType>
+bool loess_helper(PyObject *soretd_x_array, const uint32_t soretd_x_length,
+		   PyObject *soretd_y_array, 
+		   PyObject *samples_array, const uint32_t samples_length, 
+		   const uint32_t neighbours, 
+		   PyObject *out_array)
+{
+	typedef PythonTypeHelper<NpType>::Type Type;
+
+	Type *out_data = (Type*)PyArray_DATA(out_array);
+	Type *soretd_x_array_data = (Type*)PyArray_DATA(soretd_x_array);
+	Type *soretd_y_array_data = (Type*)PyArray_DATA(soretd_y_array);
+	Type *samples_array_data = (Type*)PyArray_DATA(samples_array);
+
+	return loess<Type>(soretd_x_array_data, soretd_x_length, soretd_y_array_data, samples_array_data, samples_length, neighbours, out_data);
+}
 
 /*
 	* Implements an example function.
@@ -15,162 +53,178 @@ PyDoc_STRVAR(fast_loess_doc, "loess(soretd_x, soretd_y, samples, neighbours)\
 \
 Example function");
 
-	PyObject *fast_loess(PyObject *self, PyObject *args)
+PyObject *fast_loess(PyObject *self, PyObject *args)
+{
+	/* Shared references that do not need Py_DECREF before returning. */
+	int neighbours = 0;
+	PyObject *soretd_x = NULL;
+	PyObject *soretd_y = NULL;
+	PyObject *samples = NULL;
+
+	/* Parse positional and keyword arguments */
+	static char* keywords[] = {"soretd_x", "soretd_y", "samples", "neighbours", NULL};
+	if(!PyArg_ParseTuple(args, "OOOI", &soretd_x, &soretd_y, &samples, &neighbours))
 	{
-		/* Shared references that do not need Py_DECREF before returning. */
+		return NULL;
+	}
+
+	struct DataHelper
+	{
 		PyObject *soretd_x = NULL;
 		PyObject *soretd_y = NULL;
 		PyObject *samples = NULL;
-		int neighbours = 0;
 
-		/* Parse positional and keyword arguments */
-		static char* keywords[] = {"soretd_x", "soretd_y", "samples", "neighbours", NULL};
-		if(!PyArg_ParseTuple(args, "OOOI", &soretd_x, &soretd_y, &samples, &neighbours))
-		{
-			return NULL;
-		}
+		PyObject *out_array = NULL;
 
-		if(soretd_x == NULL || soretd_y == NULL || samples == NULL || neighbours < 1)
+		~DataHelper()
 		{
-			PyErr_SetString(PyExc_TypeError, "Couldn't parse the input arrays.");
 			Py_XDECREF(soretd_x);
 			Py_XDECREF(soretd_y);
 			Py_XDECREF(samples);
-			return NULL;
-		}
-
-		if(PyArray_API == NULL)
-			import_array();
-
-		PyObject *soretd_x_array = PyArray_FROM_OTF(soretd_x, NPY_FLOAT, NPY_IN_ARRAY);
-		PyObject *soretd_y_array = PyArray_FROM_OTF(soretd_y, NPY_FLOAT, NPY_IN_ARRAY);
-		PyObject *samples_array = PyArray_FROM_OTF(samples, NPY_FLOAT, NPY_IN_ARRAY);
-
-		if(soretd_x_array == NULL || soretd_y_array == NULL || samples_array == NULL)
-		{
-			PyErr_SetString(PyExc_TypeError, "Couldn't parse the input arrays.");
-			Py_XDECREF(soretd_x_array);
-			Py_XDECREF(soretd_y_array);
-			Py_XDECREF(samples_array);
-			return NULL;
-		}
-
-		/* Check that the input arrays are 1D. */
-		int soretd_x_dim = (int)PyArray_NDIM(soretd_x_array);
-		int soretd_y_dim = (int)PyArray_NDIM(soretd_y_array);
-		int samples_dim = (int)PyArray_NDIM(samples_array);
-		if(soretd_x_dim != 1 || soretd_y_dim != 1 || samples_dim != 1)
-		{
-			PyErr_SetString(PyExc_TypeError, "The input arrays must be 1D.");
-			Py_XDECREF(soretd_x_array);
-			Py_XDECREF(soretd_y_array);
-			Py_XDECREF(samples_array);
-			return NULL;
-		}
-
-		/* Get the lengths of the outputs. */
-		int soretd_x_length = (int)PyArray_DIM(soretd_x_array, 0);
-		int soretd_y_length = (int)PyArray_DIM(soretd_y_array, 0);
-		if(soretd_x_length != soretd_y_length)
-		{
-			PyErr_SetString(PyExc_TypeError, "sorted_x and sorted_y must be same length.");
-			Py_XDECREF(soretd_x_array);
-			Py_XDECREF(soretd_y_array);
-			Py_XDECREF(samples_array);
-			return NULL;
-		}
-
-		int samples_length = (int)PyArray_DIM(samples_array, 0);
-		if(samples_length < 1)
-		{
-			PyErr_SetString(PyExc_TypeError, "samples should be not empty.");
-			Py_XDECREF(soretd_x_array);
-			Py_XDECREF(soretd_y_array);
-			Py_XDECREF(samples_array);
-			return NULL;
-		}
-
-		/* Build the output array. */
-		npy_intp dims{samples_length};
-		PyObject *out_array = PyArray_SimpleNew(1, &dims, NPY_FLOAT);
-		if(out_array == NULL)
-		{
-			PyErr_SetString(PyExc_RuntimeError, "Couldn't build output array.");
-			Py_XDECREF(soretd_x_array);
-			Py_XDECREF(soretd_y_array);
-			Py_XDECREF(samples_array);
 			Py_XDECREF(out_array);
-			return NULL;
 		}
-		float *out_data = (float*)PyArray_DATA(out_array);
+	} data_helper{soretd_x, soretd_y, samples, nullptr};
 
-		float *soretd_x_array_data = (float*)PyArray_DATA(soretd_x_array);
-		float *soretd_y_array_data = (float*)PyArray_DATA(soretd_y_array);
-		float *samples_array_data = (float*)PyArray_DATA(soretd_y_array);
-
-		/* Function implementation starts here */
-
-		if(!loess(soretd_x_array_data, soretd_x_length, soretd_y_array_data, samples_array_data, samples_length, neighbours, out_data))
-		{
-			PyErr_SetString(PyExc_RuntimeError, "Failed to calculate loess.");
-			Py_XDECREF(soretd_x_array);
-			Py_XDECREF(soretd_y_array);
-			Py_XDECREF(samples_array);
-			Py_XDECREF(out_array);
-			return NULL;    /* return NULL indicates error */
-		}
-
-		return out_array;
-	}
-
-	/*
-	 * List of functions to add to fast_loess in exec_fast_loess().
-	 */
-	static PyMethodDef fast_loess_functions[] = {
-		{ "loess", (PyCFunction)fast_loess, METH_VARARGS, fast_loess_doc },
-		{ NULL, NULL, 0, NULL } /* marks end of array */
-	};
-
-	/*
-	 * Initialize fast_loess. May be called multiple times, so avoid
-	 * using static state.
-	 */
-	int exec_fast_loess(PyObject *module)
+	if(soretd_x == NULL || soretd_y == NULL || samples == NULL || neighbours < 1)
 	{
-		PyModule_AddFunctions(module, fast_loess_functions);
-
-		PyModule_AddStringConstant(module, "__author__", "Dmitry Gladky");
-		PyModule_AddStringConstant(module, "__version__", "0.0.1");
-		PyModule_AddIntConstant(module, "year", 2018);
-
-		return 0; /* success */
+		PyErr_SetString(PyExc_TypeError, "Couldn't parse the input arrays.");
+		return NULL;
 	}
 
-	/*
-	 * Documentation for fast_stl_impl.
-	 */
-	PyDoc_STRVAR(fast_stl_doc, "The fast_stl_impl module");
+	if(PyArray_API == NULL)
+		import_array();
 
-
-	static PyModuleDef_Slot fast_loess_slots[] = {
-		{ Py_mod_exec, exec_fast_loess },
-		{ 0, NULL }
-	};
-
-	static PyModuleDef fast_stl_impl_def = {
-		PyModuleDef_HEAD_INIT,
-		"fast_stl_impl",
-		fast_stl_doc,
-		0,              /* m_size */
-		NULL,           /* m_methods */
-		fast_loess_slots,
-		NULL,           /* m_traverse */
-		NULL,           /* m_clear */
-		NULL,           /* m_free */
-	};
-
-	PyMODINIT_FUNC PyInit_fast_stl_impl()
+	const NPY_TYPES array_type = static_cast<NPY_TYPES>(PyArray_TYPE(soretd_x));
+	if(!(array_type == NPY_FLOAT ||
+		array_type == NPY_CFLOAT ||
+		array_type == NPY_DOUBLE ||
+		array_type == NPY_CDOUBLE))
 	{
-		return PyModuleDef_Init(&fast_stl_impl_def);
+		PyErr_SetString(PyExc_TypeError, "Invalid array types.");
+		return NULL;
 	}
+
+	PyObject *soretd_x_array = PyArray_FROM_OTF(soretd_x, array_type, NPY_IN_ARRAY);
+	PyObject *soretd_y_array = PyArray_FROM_OTF(soretd_y, array_type, NPY_IN_ARRAY);
+	PyObject *samples_array = PyArray_FROM_OTF(samples, array_type, NPY_IN_ARRAY);
+
+	if(soretd_x_array == NULL || soretd_y_array == NULL || samples_array == NULL)
+	{
+		PyErr_SetString(PyExc_TypeError, "Couldn't parse the input arrays.");
+		return NULL;
+	}
+
+	/* Check that the input arrays are 1D. */
+	int soretd_x_dim = (int)PyArray_NDIM(soretd_x_array);
+	int soretd_y_dim = (int)PyArray_NDIM(soretd_y_array);
+	int samples_dim = (int)PyArray_NDIM(samples_array);
+	if(soretd_x_dim != 1 || soretd_y_dim != 1 || samples_dim != 1)
+	{
+		PyErr_SetString(PyExc_TypeError, "The input arrays must be 1D.");
+		return NULL;
+	}
+
+	/* Get the lengths of the outputs. */
+	int soretd_x_length = (int)PyArray_DIM(soretd_x_array, 0);
+	int soretd_y_length = (int)PyArray_DIM(soretd_y_array, 0);
+	if(soretd_x_length != soretd_y_length)
+	{
+		PyErr_SetString(PyExc_TypeError, "sorted_x and sorted_y must be same length.");
+		return NULL;
+	}
+
+	int samples_length = (int)PyArray_DIM(samples_array, 0);
+	if(samples_length < 1)
+	{
+		PyErr_SetString(PyExc_TypeError, "samples should be not empty.");
+		return NULL;
+	}
+
+	/* Build the output array. */
+	npy_intp dims{samples_length};
+	PyObject *out_array = PyArray_SimpleNew(1, &dims, array_type);
+	if(out_array == NULL)
+	{
+		PyErr_SetString(PyExc_RuntimeError, "Couldn't build output array.");
+		return NULL;
+	}		
+	data_helper.out_array = out_array;
+
+	bool result = false;
+	switch(array_type)
+	{
+	case NPY_FLOAT:
+		result = loess_helper<NPY_FLOAT>(soretd_x_array, soretd_x_length, soretd_y_array, samples_array, samples_length, neighbours, out_array);
+		break;
+	case NPY_CFLOAT:
+		result = loess_helper<NPY_CFLOAT>(soretd_x_array, soretd_x_length, soretd_y_array, samples_array, samples_length, neighbours, out_array);
+		break;
+	case NPY_DOUBLE:
+		result = loess_helper<NPY_DOUBLE>(soretd_x_array, soretd_x_length, soretd_y_array, samples_array, samples_length, neighbours, out_array);
+		break;
+	case NPY_CDOUBLE:
+		result = loess_helper<NPY_CDOUBLE>(soretd_x_array, soretd_x_length, soretd_y_array, samples_array, samples_length, neighbours, out_array);
+		break;
+	default:
+		break;
+	}
+
+	if(!result)
+	{
+		PyErr_SetString(PyExc_RuntimeError, "Failed to calculate loess.");
+		return NULL;    /* return NULL indicates error */
+	}
+
+	return out_array;
+}
+
+/*
+	* List of functions to add to fast_loess in exec_fast_loess().
+	*/
+static PyMethodDef fast_loess_functions[] = {
+	{ "loess", (PyCFunction)fast_loess, METH_VARARGS, fast_loess_doc },
+	{ NULL, NULL, 0, NULL } /* marks end of array */
+};
+
+/*
+	* Initialize fast_loess. May be called multiple times, so avoid
+	* using static state.
+	*/
+int exec_fast_loess(PyObject *module)
+{
+	PyModule_AddFunctions(module, fast_loess_functions);
+
+	PyModule_AddStringConstant(module, "__author__", "Dmitry Gladky");
+	PyModule_AddStringConstant(module, "__version__", "0.0.1");
+	PyModule_AddIntConstant(module, "year", 2018);
+
+	return 0; /* success */
+}
+
+/*
+	* Documentation for fast_stl_impl.
+	*/
+PyDoc_STRVAR(fast_stl_doc, "The fast_stl_impl module");
+
+
+static PyModuleDef_Slot fast_loess_slots[] = {
+	{ Py_mod_exec, exec_fast_loess },
+	{ 0, NULL }
+};
+
+static PyModuleDef fast_stl_impl_def = {
+	PyModuleDef_HEAD_INIT,
+	"fast_stl_impl",
+	fast_stl_doc,
+	0,              /* m_size */
+	NULL,           /* m_methods */
+	fast_loess_slots,
+	NULL,           /* m_traverse */
+	NULL,           /* m_clear */
+	NULL,           /* m_free */
+};
+
+PyMODINIT_FUNC PyInit_fast_stl_impl()
+{
+	return PyModuleDef_Init(&fast_stl_impl_def);
 }
--- fast_stl/src/loess.h
+++ fast_stl/src/loess.h
@@ -5,11 +5,12 @@
 #include <stdio.h>
 #include <algorithm>
 
+template <typename T>
 struct WindowIterator
 {
-	const float *x;
-	const float *y;
-	float *distance;
+	const T *x;
+	const T *y;
+	T *distance;
 
 	bool operator<(const WindowIterator& rht) const
 	{
@@ -31,7 +32,8 @@ struct WindowIterator
 	}
 };
 
-WindowIterator operator+(const WindowIterator& value, const uint32_t advance)
+template <typename T>
+WindowIterator<T> operator+(const WindowIterator<T>& value, const uint32_t advance)
 {
 	return {
 		value.x + advance,
@@ -40,7 +42,8 @@ WindowIterator operator+(const WindowIterator& value, const uint32_t advance)
 	};
 }
 
-WindowIterator operator-(const WindowIterator& value, const uint32_t advance)
+template <typename T>
+WindowIterator<T> operator-(const WindowIterator<T>& value, const uint32_t advance)
 {
 	return {
 		value.x - advance,
@@ -49,7 +52,8 @@ WindowIterator operator-(const WindowIterator& value, const uint32_t advance)
 	};
 }
 
-void find_neighbours(WindowIterator &window_first, WindowIterator &window_last, const WindowIterator &last, const uint32_t neighbours, const float pivot)
+template <typename T>
+void find_neighbours(WindowIterator<T> &window_first, WindowIterator<T> &window_last, const WindowIterator<T> &last, const uint32_t neighbours, const T pivot)
 {
 	// move window to the left if too close to the last
 	if(last - neighbours < window_first)
@@ -65,9 +69,9 @@ void find_neighbours(WindowIterator &window_first, WindowIterator &window_last, 
 	}
 
 	// fill distances in last window
-	for(WindowIterator cursor = window_first; cursor <= window_last; ++cursor)
+	for(WindowIterator<T> cursor = window_first; cursor <= window_last; ++cursor)
 	{
-		float& distance = *cursor.distance;
+		T& distance = *cursor.distance;
 
 		bool is_pivot_grater = *cursor.x < pivot;
 		if(is_pivot_grater)
@@ -76,15 +80,15 @@ void find_neighbours(WindowIterator &window_first, WindowIterator &window_last, 
 			distance = *cursor.x - pivot;
 	}
 
-	float current_window_length = *window_first.distance + *window_last.distance;
+	T current_window_length = *window_first.distance + *window_last.distance;
 
 	// find smallest distance window
 	while(window_last < last)
 	{
-		const float next_left_distance = pivot - *window_first.x;
-		const float next_right_distance = *window_last.x - pivot;
+		const T next_left_distance = pivot - *window_first.x;
+		const T next_right_distance = *window_last.x - pivot;
 
-		const float next_window_length = next_left_distance + next_right_distance;
+		const T next_window_length = next_left_distance + next_right_distance;
 
 		// move window
 		if(next_window_length < current_window_length)
@@ -103,28 +107,29 @@ void find_neighbours(WindowIterator &window_first, WindowIterator &window_last, 
 	}
 }
 
-float calculate(const WindowIterator &window_first, const WindowIterator &window_last, float *weights)
+template <typename T>
+T calculate(const WindowIterator<T> &window_first, const WindowIterator<T> &window_last, T *weights)
 {
-	float weight_sum{0.0f};
+	T weight_sum{0.0f};
 
-	float *current_weight = weights;
-	const float max_distance = std::max(*window_first.distance, *(window_last.distance));
-	for(WindowIterator cursor = window_first; cursor <= window_last; ++cursor, ++current_weight)
+	T *current_weight = weights;
+	const T max_distance = std::max(*window_first.distance, *(window_last.distance));
+	for(WindowIterator<T> cursor = window_first; cursor <= window_last; ++cursor, ++current_weight)
 	{
-		const float &distance = *cursor.distance;
-		float &weight = *current_weight;
+		const T &distance = *cursor.distance;
+		T &weight = *current_weight;
 
-		const float scaled_distance = distance / max_distance;
-		weight = std::min(1.0f, std::max(0.0f, scaled_distance));
+		const T scaled_distance = distance / max_distance;
+		weight = std::min(T{1.0}, std::max(T{0.0}, scaled_distance));
 
 		weight = (1 - weight * weight  * weight); // (1 - weight ^ 3)
 		weight = weight * weight * weight; // (1 - weight ^ 3) ^ 3
 		weight_sum += weight;
 	}
 
-	float g{0};
+	T g{0};
 	current_weight = weights;
-	for(WindowIterator cursor = window_first; cursor <= window_last; ++cursor, ++current_weight)
+	for(WindowIterator<T> cursor = window_first; cursor <= window_last; ++cursor, ++current_weight)
 	{
 		g += (*cursor.y) * (*current_weight);
 	}
@@ -134,34 +139,32 @@ float calculate(const WindowIterator &window_first, const WindowIterator &window
 	return g;
 }
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-	bool loess(const float *soretd_x_begin, const uint32_t soretd_x_size, const float *soretd_y_begin, const float *sample_x_begin, const uint32_t num_samples, uint32_t neighbours, float *g)
+template <typename T>
+bool loess(const T *soretd_x_begin, const uint32_t soretd_x_size, const T *soretd_y_begin, const T *sample_x_begin, const uint32_t num_samples, uint32_t neighbours, T *g)
+{
+	if(!soretd_x_begin || !soretd_y_begin || neighbours == 0)
+		return false;
+
+	if(neighbours > soretd_x_size)
+		neighbours = soretd_x_size;
+	--neighbours;
+
+	T *distances = new T[soretd_x_size];
+	T *weights = new T[neighbours + 1];
+
+	WindowIterator<T> window_first{soretd_x_begin, soretd_y_begin, distances};
+	WindowIterator<T> window_last(window_first + neighbours);
+
+	const WindowIterator<T> last{&*(soretd_x_begin + soretd_x_size - 1), nullptr, nullptr};
+
+	for(uint32_t iteration = 0; iteration < num_samples; ++iteration)
 	{
-		if(!soretd_x_begin || !soretd_y_begin || neighbours == 0)
-			return false;
+		find_neighbours(window_first, window_last, last, neighbours, sample_x_begin[iteration]);
 
-		if(neighbours > soretd_x_size)
-			neighbours = soretd_x_size;
-		--neighbours;
-
-		float *distances = new float[soretd_x_size];
-		float *weights = new float[neighbours + 1];
-
-		WindowIterator window_first{soretd_x_begin, soretd_y_begin, distances};
-		WindowIterator window_last(window_first + neighbours);
-
-		const WindowIterator last{&*(soretd_x_begin + soretd_x_size - 1), nullptr, nullptr};
-
-		for(uint32_t iteration = 0; iteration < num_samples; ++iteration)
-		{
-			find_neighbours(window_first, window_last, last, neighbours, sample_x_begin[iteration]);
-
-			g[iteration] = calculate(window_first, window_last, weights);
-		}
-
-		return true;
+		g[iteration] = calculate(window_first, window_last, weights);
 	}
+
+	return true;
 }
+
 #endif // c_loess_h__
